29ac00733d4523442a112235fbaf95e0416a6ff1
VirginieT <vtouzalin33@gmail.com>
2022-03-09 10:10:25 +0100

    modif couleu btn + dynamisation home

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -33,7 +33,7 @@ class AppFixtures extends Fixture
             'Brosse',
             'Boule à zéro',
             'Carré',
-            'Couette',
+            'Couettes',
             'Dreadlocks',
         ];
 
