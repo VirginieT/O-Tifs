1402cf6c3a2b72f051e95363dc1adb8a9148849e
VirginieT <vtouzalin33@gmail.com>
2022-09-20 23:39:13 +0200

    add changes in templates + connection

---
+++ migrations/Version20220920194951.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220920194951 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE coiffure (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE person (id INT AUTO_INCREMENT NOT NULL, coiffure_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, INDEX IDX_34DCD176C17ACDFA (coiffure_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE person ADD CONSTRAINT FK_34DCD176C17ACDFA FOREIGN KEY (coiffure_id) REFERENCES coiffure (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE person DROP FOREIGN KEY FK_34DCD176C17ACDFA');
+        $this->addSql('DROP TABLE coiffure');
+        $this->addSql('DROP TABLE person');
+    }
+}
